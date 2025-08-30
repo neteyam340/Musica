5eda9c688c609fe34e2e768022faf4e06db3d52f
neteyam340 <granjahervyn@gmail.com>
2025-08-30 13:56:51 -0500

    valeria

--- MiPlaylist.java
+++ MiPlaylist.java
@@ -1,12 +1,12 @@
 public class MiPlaylist {
     public static void main(String[] args) {
         
-        Cancion cancion1 = new Cancion("Bohemian Rhapsody", "Queen", 355);
-        Cancion cancion2 = new Cancion("Stairway to Heaven", "Led Zeppelin", 482);
-        Cancion cancion3 = new Cancion("Hotel California", "Eagles", 391);
+        Cancion cancion1 = new Cancion("ba ba bad", "Ryan castro", 227);
+        Cancion cancion2 = new Cancion("i miss you", "beele", 254);
+        Cancion cancion3 = new Cancion("not like us", "kendrick lamar", 434);
 
         
-        System.out.println("--- Mi Playlist ---");
+        System.out.println("--- Mi Playlist Valeria ---");
         cancion1.reproducir();
         cancion2.reproducir();
         cancion3.reproducir();
